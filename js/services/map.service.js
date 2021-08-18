4c9fedf52bf87099a714ecc1f68eb22863cd4549
shaharmeshulam <shahar.mesh@gmail.com>
2021-08-18 13:52:01 +0300

    added weather

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAcNnUa6LHjI2hgPgHhAfAYW5j7-dxYVWs'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
