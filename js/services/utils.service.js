bcbada8395fdb3c52ad7b4aae39f6e7ad97df8ce
shaharmeshulam <shahar.mesh@gmail.com>
2021-08-18 19:26:35 +0300

    comment fix

--- js/services/utils.service.js
+++ js/services/utils.service.js
@@ -1,9 +1,9 @@
-// This function provides a Promise API to the callback-based-api of getCurrentPosition
 export const utilsService = {
     getPosition,
     formatCelsius
 }
 
+// This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
