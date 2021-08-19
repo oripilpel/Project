bbd977db22eecbbc0cb082bf2fa92f2d8f3d5f66
shaharmeshulam <shahar.mesh@gmail.com>
2021-08-19 08:34:31 +0300

    fix

--- js/app.controller.js
+++ js/app.controller.js
@@ -18,10 +18,10 @@ function onInit() {
     if (!params.lng || !params.lat) {
         params.lat = 32.0749831;
         params.lng = 34.9120554;
+        window.history.pushState('', '', `?lat=${params.lat}&lng=${params.lng}`);
     }
-    window.history.pushState('', '', `?lat=${params.lat}&lng=${params.lng}`);
     Promise.all([mapService.initMap(+params.lat, +params.lng), mapService.addMarkers(locService.getLocs())])
-        .then(() => onGetWeather({ lat: params.lat, lng: params.lng }))
+        .then(() => onGetWeather({ lat: +params.lat, lng: +params.lng }))
         .catch(() => console.log('Error: cannot init map'));
 }
 
